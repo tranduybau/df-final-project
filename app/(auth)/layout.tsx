ca141e518232581e08de09818020ab36d0d3b8d3
tranduybau <trdbau@gmail.com>
2023-10-30 01:10:21 +0700

    [task] update layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,22 @@
+'use client';
+
 import React from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import ROUTES from '@/constants/ROUTES';
+import { useAuthContext } from '@/context/auth';
 
 export default function AuthLayout({ children }: React.PropsWithChildren) {
+  const { push } = useRouter();
+  const { isLogin } = useAuthContext();
+  const searchParams = useSearchParams();
+
+  React.useEffect(() => {
+    if (isLogin) {
+      const search = searchParams.get('redirect');
+      push(search || ROUTES.HOME);
+    }
+  }, [isLogin, push, searchParams]);
+
   return <div>{children}</div>;
 }
